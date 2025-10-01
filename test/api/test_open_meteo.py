1f199e147889914e24f1d6d154479948bc9c7a14
Klaas <klaas@klaasnotfound.com>
2025-10-01 23:14:41 +0200

    Rename, reformat, update fixtures

--- test/api/test_open_meteo.py
+++ test/api/test_open_meteo.py
@@ -8,9 +8,9 @@ from src.util.geo import LatLon
 def test_get_forecast():
     fc = get_forecast(52.52, 13.41)
     assert (fc.lat, fc.lon) == (52.52, 13.419998)
-    assert [d.isoformat()[:10] for d in fc.days[:2]] == ["2025-09-17", "2025-09-18"]
-    assert fc.daylight_dur[:2] == [45132.20, 44888.03]
-    assert fc.sunshine_dur[:2] == [29070.21, 0.00]
+    assert [d.isoformat()[:10] for d in fc.days[:2]] == ["2025-10-01", "2025-10-02"]
+    assert fc.daylight_dur[:2] == [41732.23, 41486.97]
+    assert fc.sunshine_dur[:2] == [18602.27, 36000.0]
 
 
 @pytest.mark.vcr
@@ -23,9 +23,9 @@ def test_get_forecasts():
         ]
     )
     assert len(fcs) == 3
-    assert [d.isoformat()[:10] for d in fcs[0].days[:2]] == ["2025-09-17", "2025-09-18"]
-    assert [d.isoformat()[:10] for d in fcs[1].days[:2]] == ["2025-09-17", "2025-09-18"]
-    assert [d.isoformat()[:10] for d in fcs[2].days[:2]] == ["2025-09-17", "2025-09-18"]
+    assert [d.isoformat()[:10] for d in fcs[0].days[:2]] == ["2025-10-01", "2025-10-02"]
+    assert [d.isoformat()[:10] for d in fcs[1].days[:2]] == ["2025-10-01", "2025-10-02"]
+    assert [d.isoformat()[:10] for d in fcs[2].days[:2]] == ["2025-10-01", "2025-10-02"]
     assert (fcs[0].lat, fcs[0].lon) == (48.66, 9)
     assert (fcs[1].lat, fcs[1].lon) == (48.92, 11.4)
     assert (fcs[2].lat, fcs[2].lon) == (52.5, 13.419998)
--- test/model/open_meteo_test.py
+++ test/model/open_meteo_test.py
@@ -7,13 +7,21 @@ from src.model.open_meteo import OpenMeteoForecast
 def test_init():
     # Rejects invalid values
     with pytest.raises(AssertionError):
-        OpenMeteoForecast(-100, 0, [], {"daylight_duration": [], "sunshine_duration": []})
+        OpenMeteoForecast(
+            -100, 0, [], {"daylight_duration": [], "sunshine_duration": []}
+        )
     with pytest.raises(AssertionError):
-        OpenMeteoForecast(100, 0, [], {"daylight_duration": [], "sunshine_duration": []})
+        OpenMeteoForecast(
+            100, 0, [], {"daylight_duration": [], "sunshine_duration": []}
+        )
     with pytest.raises(AssertionError):
-        OpenMeteoForecast(0, -200, [], {"daylight_duration": [], "sunshine_duration": []})
+        OpenMeteoForecast(
+            0, -200, [], {"daylight_duration": [], "sunshine_duration": []}
+        )
     with pytest.raises(AssertionError):
-        OpenMeteoForecast(0, 200, [], {"daylight_duration": [], "sunshine_duration": []})
+        OpenMeteoForecast(
+            0, 200, [], {"daylight_duration": [], "sunshine_duration": []}
+        )
     with pytest.raises(AssertionError):
         OpenMeteoForecast(0, 0, [], {"sunshine_duration": []})
     with pytest.raises(AssertionError):
@@ -21,9 +29,19 @@ def test_init():
     with pytest.raises(AssertionError):
         OpenMeteoForecast(0, 0, [], {"daylight_duration": [], "sunshine_duration": []})
     with pytest.raises(AssertionError):
-        OpenMeteoForecast(0, 0, [parse("2025-09-17")], {"daylight_duration": [], "sunshine_duration": []})
+        OpenMeteoForecast(
+            0,
+            0,
+            [parse("2025-09-17")],
+            {"daylight_duration": [], "sunshine_duration": []},
+        )
     with pytest.raises(AssertionError):
-        OpenMeteoForecast(0, 0, [parse("2025-09-17")], {"daylight_duration": [1], "sunshine_duration": []})
+        OpenMeteoForecast(
+            0,
+            0,
+            [parse("2025-09-17")],
+            {"daylight_duration": [1], "sunshine_duration": []},
+        )
 
     # Works as expected
     fc = OpenMeteoForecast(
@@ -37,16 +55,28 @@ def test_init():
         {"daylight_duration": [1, 2, 3], "sunshine_duration": [4, 5, 6]},
     )
     assert (fc.lat, fc.lon) == (0, 0)
-    assert [d.isoformat()[:10] for d in fc.days] == ["2025-09-17", "2025-09-18", "2025-09-19"]
+    assert [d.isoformat()[:10] for d in fc.days] == [
+        "2025-09-17",
+        "2025-09-18",
+        "2025-09-19",
+    ]
     assert fc.daylight_dur == [1, 2, 3]
     assert fc.sunshine_dur == [4, 5, 6]
 
 
 def test_repr():
-    fc = OpenMeteoForecast(45, 30, [parse("2025-09-19")], {"daylight_duration": [1], "sunshine_duration": [4]})
+    fc = OpenMeteoForecast(
+        45,
+        30,
+        [parse("2025-09-19")],
+        {"daylight_duration": [1], "sunshine_duration": [4]},
+    )
     assert f"{fc}" == "OpenMeteoForecast (45, 30) [1 day]"
     fc = OpenMeteoForecast(
-        45, 30, [parse("2025-09-19"), parse("2025-09-20")], {"daylight_duration": [1, 2], "sunshine_duration": [4, 5]}
+        45,
+        30,
+        [parse("2025-09-19"), parse("2025-09-20")],
+        {"daylight_duration": [1, 2], "sunshine_duration": [4, 5]},
     )
     assert f"{fc}" == "OpenMeteoForecast (45, 30) [2 days]"
 
@@ -69,10 +99,38 @@ def json_data():
         "timezone": "Europe/Berlin",
         "timezone_abbreviation": "GMT+2",
         "elevation": 38,
-        "daily_units": {"time": "iso8601", "daylight_duration": "s", "sunshine_duration": "s"},
+        "daily_units": {
+            "time": "iso8601",
+            "daylight_duration": "s",
+            "sunshine_duration": "s",
+        },
         "daily": {
-            "time": ["2025-09-16", "2025-09-17", "2025-09-18", "2025-09-19", "2025-09-20", "2025-09-21", "2025-09-22"],
-            "daylight_duration": [45397.04, 45152.57, 44908.36, 44664.55, 44421.27, 44178.66, 43936.83],
-            "sunshine_duration": [8741.57, 30228.32, 134.59, 36776.93, 37313.67, 38041.49, 21572.05],
+            "time": [
+                "2025-09-16",
+                "2025-09-17",
+                "2025-09-18",
+                "2025-09-19",
+                "2025-09-20",
+                "2025-09-21",
+                "2025-09-22",
+            ],
+            "daylight_duration": [
+                45397.04,
+                45152.57,
+                44908.36,
+                44664.55,
+                44421.27,
+                44178.66,
+                43936.83,
+            ],
+            "sunshine_duration": [
+                8741.57,
+                30228.32,
+                134.59,
+                36776.93,
+                37313.67,
+                38041.49,
+                21572.05,
+            ],
         },
     }
