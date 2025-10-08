58ff39cb407cd63f872b90ed2b106f08a7df9e2e
Klaas <klaas@klaasnotfound.com>
2025-10-08 12:16:08 +0200

    Add historical forecast fetching

--- src/api/open_meteo.py
+++ src/api/open_meteo.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 
 from src.model.open_meteo import (
@@ -27,6 +29,40 @@ def get_forecasts(locs: list[LatLon]) -> list[OpenMeteoForecast]:
     lats = ",".join(map(str, [loc.lat for loc in locs]))
     lons = ",".join(map(str, [loc.lon for loc in locs]))
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lats}&longitude={lons}&hourly={HOURLY_ATTRS}&tilt=35"
+    res = requests.get(url)
+    res.raise_for_status()
+    data: list[OpenMeteoForecastData] = res.json()
+
+    forecasts = [OpenMeteoForecast.fromjson(d) for d in data]
+
+    return forecasts
+
+
+def get_historical_forecast(
+    lat: float, lon: float, start: datetime, end: datetime
+) -> OpenMeteoForecast:
+    """Fetch historic OpenMeteo weather forecast for a given location and time range."""
+
+    sd = start.isoformat()[:10]
+    ed = end.isoformat()[:10]
+    url = f"https://historical-forecast-api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&start_date={sd}&end_date={ed}&hourly={HOURLY_ATTRS}&tilt=35"
+    res = requests.get(url)
+    data: OpenMeteoForecastData = res.json()
+    forecast = OpenMeteoForecast.fromjson(data)
+
+    return forecast
+
+
+def get_historical_forecasts(
+    locs: list[LatLon], start: datetime, end: datetime
+) -> list[OpenMeteoForecast]:
+    """Fetch OpenMeteo weather forecasts for several locations and a given time range."""
+
+    lats = ",".join(map(str, [loc.lat for loc in locs]))
+    lons = ",".join(map(str, [loc.lon for loc in locs]))
+    sd = start.isoformat()[:10]
+    ed = end.isoformat()[:10]
+    url = f"https://historical-forecast-api.open-meteo.com/v1/forecast?latitude={lats}&longitude={lons}&start_date={sd}&end_date={ed}&hourly={HOURLY_ATTRS}&tilt=35"
     res = requests.get(url)
     res.raise_for_status()
     data: list[OpenMeteoForecastData] = res.json()
--- test/api/test_open_meteo.py
+++ test/api/test_open_meteo.py
@@ -1,6 +1,12 @@
 import pytest
+from dateutil.parser import parse
 
-from src.api.open_meteo import get_forecast, get_forecasts
+from src.api.open_meteo import (
+    get_forecast,
+    get_forecasts,
+    get_historical_forecast,
+    get_historical_forecasts,
+)
 from src.util.geo import LatLon
 
 
@@ -26,18 +32,48 @@ def test_get_forecasts():
         ]
     )
     assert len(fcs) == 3
-    assert [d.isoformat()[:16] for d in fcs[0].times[:2]] == [
-        "2025-10-08T00:00",
-        "2025-10-08T01:00",
-    ]
-    assert [d.isoformat()[:16] for d in fcs[1].times[:2]] == [
-        "2025-10-08T00:00",
-        "2025-10-08T01:00",
-    ]
-    assert [d.isoformat()[:16] for d in fcs[2].times[:2]] == [
-        "2025-10-08T00:00",
-        "2025-10-08T01:00",
-    ]
+    for fcidx in range(0, 3):
+        assert [d.isoformat()[:16] for d in fcs[fcidx].times[:2]] == [
+            "2025-10-08T00:00",
+            "2025-10-08T01:00",
+        ]
     assert (fcs[0].lat, fcs[0].lon) == (48.66, 9)
     assert (fcs[1].lat, fcs[1].lon) == (48.92, 11.4)
     assert (fcs[2].lat, fcs[2].lon) == (52.5, 13.419998)
+
+
+@pytest.mark.vcr
+def test_get_historical_forecast():
+    fc = get_historical_forecast(52.52, 13.41, parse("2023-01-01"), parse("2023-01-10"))
+    assert (fc.lat, fc.lon) == (52.52, 13.419998)
+    assert [d.isoformat()[:16] for d in fc.times[:2]] == [
+        "2023-01-01T00:00",
+        "2023-01-01T01:00",
+    ]
+    assert getattr(fc, "temperature_2m")[:5] == [15.9, 15.6, 15.3, 15.4, 15.2]
+    assert getattr(fc, "cloud_cover_mid")[:7] == [48, 0, 0, 0, 0, 4, 98]
+
+
+@pytest.mark.vcr
+def test_get_historical_forecasts():
+    fcs = get_historical_forecasts(
+        [
+            LatLon(48.662587, 9.00161),
+            LatLon(48.917408, 11.4061),
+            LatLon(52.505774, 13.4244),
+        ],
+        parse("2023-01-01"),
+        parse("2023-01-10"),
+    )
+    assert len(fcs) == 3
+    for fcidx in range(0, 3):
+        assert [d.isoformat()[:16] for d in fcs[fcidx].times[:2]] == [
+            "2023-01-01T00:00",
+            "2023-01-01T01:00",
+        ]
+    assert (fcs[0].lat, fcs[0].lon) == (48.66, 9)
+    assert (fcs[1].lat, fcs[1].lon) == (48.92, 11.4)
+    assert (fcs[2].lat, fcs[2].lon) == (52.5, 13.419998)
+    assert getattr(fcs[0], "temperature_2m")[:5] == [11.7, 11.4, 11.3, 11.2, 11.0]
+    assert getattr(fcs[1], "temperature_2m")[:5] == [9.3, 9.3, 8.9, 7.7, 7.8]
+    assert getattr(fcs[2], "temperature_2m")[:5] == [16.0, 15.7, 15.4, 15.5, 15.2]
